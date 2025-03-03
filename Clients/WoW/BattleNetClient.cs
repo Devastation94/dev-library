6f787927acd8b09eb3a6e9ccec63592b39e24a53
Al <alboudreau1994@gmail.com>
2025-03-02 19:04:54 -0500

    updates

--- Clients/WoW/BattleNetClient.cs
+++ Clients/WoW/BattleNetClient.cs
@@ -31,7 +31,7 @@ namespace dev_refined.Clients
         public async Task<BlizzardRealmResponse> GetServerInformation(string token)
         {
             using var client = new HttpClient();
-            using var request = new HttpRequestMessage(new HttpMethod("GET"), realmDataEndpoint);
+            using var request = new HttpRequestMessage(new HttpMethod("GET"), AppSettings.BasePath + realmDataEndpoint);
             request.Headers.TryAddWithoutValidation("accept", "application/json");
             request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {token}");
 
@@ -43,7 +43,7 @@ namespace dev_refined.Clients
         public async Task<string> GetItemName(string token, string itemId)
         {
             using var client = new HttpClient();
-            using var request = new HttpRequestMessage(new HttpMethod("GET"), string.Format(itemNameEndpoint, itemId));
+            using var request = new HttpRequestMessage(new HttpMethod("GET"), string.Format(AppSettings.BasePath + itemNameEndpoint, itemId));
             request.Headers.TryAddWithoutValidation("accept", "application/json");
             request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {token}");
 
