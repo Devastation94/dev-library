48db8ea2c8b0048c6efb08dfd85dde032991ab50
Al <alboudreau1994@gmail.com>
2025-04-22 15:27:54 -0400

    updates

--- Clients/WoW/RealmClient.cs
+++ Clients/WoW/RealmClient.cs
@@ -38,7 +38,7 @@ namespace dev_refined
                     }
                     else
                     {
-                        Log.Information($"Server status has not changed from {cachedData.Status.Name}");
+                        Log.Information($"Server status has not changed from {realmData.Status.Name}");
                     }
 
                     realmStatus = realmData.Status.Name.ToUpper() == "UP";
