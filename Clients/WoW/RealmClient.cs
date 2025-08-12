0eae981b65ed4ec447f29097e4989a8c63658769
Al <alboudreau1994@gmail.com>
2025-08-12 12:18:36 -0400

    updates

--- Clients/WoW/RealmClient.cs
+++ Clients/WoW/RealmClient.cs
@@ -37,6 +37,7 @@ namespace dev_refined
                 else
                 {
                     Log.Information($"Server status has not changed from {realmData.Status.Name}");
+                    File.WriteAllText(fileLocation, JsonConvert.SerializeObject(realmData));
                     return false;
                 }
             }
